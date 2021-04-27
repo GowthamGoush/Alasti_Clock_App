b9ee0af4528069c0bddb750047f08b2587433a8c
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-04-27 22:24:43 +0530

    Replace tab with bottom navigation

--- app/src/main/java/com/example/alasti/MainActivity.java
+++ app/src/main/java/com/example/alasti/MainActivity.java
@@ -1,25 +1,94 @@
 package com.example.alasti;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.alasti.Adapters.SectionsPagerAdapter;
-import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MeowBottomNavigation bottomNavigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
+        final ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setOffscreenPageLimit(3);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+
+        bottomNavigation = findViewById(R.id.bottomNavigationBar);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(R.string.Alarm_ID, R.drawable.ic_alarm));
+        bottomNavigation.add(new MeowBottomNavigation.Model(R.string.Timer_ID, R.drawable.ic_timer));
+        bottomNavigation.add(new MeowBottomNavigation.Model(R.string.Stopwatch_ID, R.drawable.ic_stopwatch));
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "clicked item : " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "clicked item : " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+
+                switch (item.getId()) {
+                    case R.string.Alarm_ID:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.string.Stopwatch_ID:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.string.Timer_ID:
+                        viewPager.setCurrentItem(3);
+                        break;
+                }
+            }
+        });
+
+        bottomNavigation.show(R.string.Alarm_ID, false);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                switch (position) {
+                    case 0:
+                        bottomNavigation.show(R.string.Alarm_ID, true);
+                        break;
+                    case 1:
+                        bottomNavigation.show(R.string.Timer_ID, true);
+                        break;
+                    case 2:
+                        bottomNavigation.show(R.string.Stopwatch_ID, true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
 
